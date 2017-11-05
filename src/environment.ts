442183888d305796086dcaf78b1c3762547b36f7
Léo Santos <aleotory@gmail.com>
2017-11-05 13:42:23 -0300

    Configurar apiBAsePath para production

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,4 @@
 export const emailRegexp:RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const apiBasePath:string = "http://localhost:3000";
-
-// export const apiBasePath:string = "https://apismack.herokuapp.com"; // Production
+// export const apiBasePath:string = "http://localhost:3000"; // Development
+export const apiBasePath:string = "https://apismack.herokuapp.com"; // Production
